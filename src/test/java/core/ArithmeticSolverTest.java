d55db198c35d9af573cd7e29f50e5e4ee86000df
ThiccDodo <33139217+ThiccDodo@users.noreply.github.com>
2018-09-13 13:22:05 -0400

    test: Added ArithemticSolver test case

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -1,5 +1,31 @@
 package core;
+import junit.framework.*;
 
-public class ArithmeticSolverTest {
-
+public class ArithmeticSolverTest extends TestCase{
+	
+	public void testAdd() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		
+		assertEquals(4, solver.add(2,2));
+	}
+	
+	public void testSubract() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(2, solver.subtract(4,2));
+	}
+	
+	public void testMultiply() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(8, solver.multiply(4,2));
+	}
+	
+	public void testDivide() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(3, solver.divide(9,3));
+	}
+	
+	public void testModulus() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(1, solver.modulus(5,2));
+	}
 }
