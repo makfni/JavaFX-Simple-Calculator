def121cd1deecc3016432663444d62c2ec4766a6
ThiccDodo <33139217+ThiccDodo@users.noreply.github.com>
2018-09-17 11:05:41 -0400

    Added: switch statement

--- src/test/java/core/ArithmeticSolver.java
+++ src/test/java/core/ArithmeticSolver.java
@@ -20,4 +20,29 @@ public class ArithmeticSolver {
 	public double modulus(double leftOperand, double rightOperand) {
 		return leftOperand % rightOperand;
 	}
+
+	public double solve(String operator, double leftOperand, double rightOperand) {
+		double answer = 0.0;
+		switch(operator) {
+			case "+":
+				answer = add(leftOperand, rightOperand);
+				break;
+			case "-":
+				answer = subtract(leftOperand, rightOperand);
+				break;
+			case "*":
+				answer = multiply(leftOperand, rightOperand);
+				break;
+			case "/":
+				answer = divide(leftOperand, rightOperand);
+				break;
+			case "%":
+				answer = modulus(leftOperand, rightOperand);
+				break;
+				
+			default:
+				break;
+			}
+		return answer;
+	}
 }
